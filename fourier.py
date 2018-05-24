db9293b7d62e333c6f0c6aec3d9293116df60e2e
ririron <townageriron@gmail.com>
2018-05-24 22:09:34 +0900

    first

---
+++ fourier.py
@@ -0,0 +1,35 @@
+from pylab import *
+from scipy.integrate import quad
+
+def fourier(fun, n_max):
+  a = []
+  b = []
+  for n in range(0, 1+n_max):
+    res, err = quad(lambda x:fun(x)*cos((2*pi*n*x)/(2*pi)), -pi, pi)
+    a.append(res/pi)
+    res, err = quad(lambda x:fun(x)*sin((2*pi*n*x)/(2*pi)), -pi, pi)
+    b.append(res/pi)
+  def fn(x):
+    sum = a[0] / 2
+    for n in range(1, 1+n_max):
+      sum += a[n]*cos((2*pi*n*x)/(2*pi)) + b[n]*sin((2*pi*n*x)/(2*pi))
+    return sum
+  return fn
+
+def f(t):
+    if -pi < t and t <= 0:
+        return -1
+    elif 0 < t and t <= pi:
+        return 1
+
+x_min = -5
+x_max = 5
+y_min = -2
+y_max = 2
+axis([x_min, x_max, y_min, y_max])
+
+f_fn = fourier(f, 2)
+xs = linspace(x_min, x_max, 256)
+plot(xs, amap(f, xs), 'b:', lw=1)
+plot(xs, amap(f_fn, xs), 'r-', lw=1)
+show()
